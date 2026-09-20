4426a5999c478c1665fe5ca3c09f43d2e1131cf2
blueGameTV <113138525+blueGameTV@users.noreply.github.com>
2026-09-20 17:16:46 +0200

    v1.1.0: load new UI assets

--- templates/shared/footer.php
+++ templates/shared/footer.php
@@ -2,8 +2,10 @@
     </div>
 </div>
 <script>window.TICKETFLOW_CSRF=<?= json_encode($csrf->token()) ?>;</script>
-<script src="assets/js/ui.js?v=0.15.2.1" defer></script>
-<script src="assets/js/live-global.js?v=0.15.2.1" defer></script>
-<script src="assets/js/global-search.js?v=0.15.2.1" defer></script>
+<script src="assets/js/ui.js?v=1.1.0" defer></script>
+<script src="assets/js/live-global.js?v=1.1.0" defer></script>
+<script src="assets/js/global-search.js?v=1.1.0" defer></script>
+<script src="assets/js/custom-selects.js?v=1.1.0" defer></script>
+<script src="assets/js/live-system-state.js?v=1.1.0" defer></script>
 </body>
 </html>
